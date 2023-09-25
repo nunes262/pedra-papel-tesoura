79527c357f25a131c19b960c342a700078bb5e16
pedronunes-devrise <pedronunes.devrise@gmail.com>
2023-09-25 20:27:29 -0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import papel from "./Assets/teste.png";
 
-function App() {
+const choices = ["Pedra", "Papel", "Tesoura"];
+
+const App = () => {
+  const [myChance, setMyChance] = useState(null);
+  const [randomChoice, setRandomChoice] = useState(null);
+  const [result, setResult] = useState(null);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+
+  const getRandomChoice = () => {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  };
+
+  const determineWinner = (user, computer) => {
+    if (user === computer) return "Empate";
+    if (
+      (user === "Pedra" && computer === "Tesoura") ||
+      (user === "Papel" && computer === "Pedra") ||
+      (user === "Tesoura" && computer === "Papel")
+    ) {
+      return "Venceu!";
+    } else {
+      return "Perdeu";
+    }
+  };
+
+  const handleUserChoice = (choice) => {
+    const computerChoice = getRandomChoice();
+    const result = determineWinner(choice, computerChoice);
+
+    setMyChance(choice);
+    setRandomChoice(computerChoice);
+    setResult(result);
+
+    if (result === "Venceu!") {
+      setPlayerScore(playerScore + 1);
+    } else if (result === "Perdeu") {
+      setComputerScore(computerScore + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pedra, Papel, Tesoura</h1>
+      <div className="game-container">
+        <div className="choices">
+          {choices.map((choice, index) => (
+            <button
+              key={choice}
+              onClick={() => handleUserChoice(choice)}
+              className={`choice-button choice-${index}`}
+            >
+              {choice}
+            </button>
+          ))}
+        </div>
+        {myChance && randomChoice && (
+          <>
+            <div className="result">
+              <div className="result-card player-card">
+                <p>Você escolheu: {myChance}</p>
+                <p>Placar do Jogador: {playerScore}</p>
+              </div>
+              <div className="result-card computer-card">
+                <p>Computador escolheu: {randomChoice}</p>
+                <p>Placar do Computador: {computerScore}</p>
+              </div>
+            </div>
+            <p className="result-text">
+              {result === "Empate" ? "Empate!" : `Você ${result}`}
+            </p>
+          </>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
